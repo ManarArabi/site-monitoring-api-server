de83643b408c327a8a3dbc77ba38e00228420df3
manarArabi <manar.araby.ma@gmail.com>
2022-01-26 22:37:41 +0200

    add validation

--- src/modules/check-entry/validation.js
+++ src/modules/check-entry/validation.js
@@ -1,5 +1,31 @@
+import Joi from 'joi'
+import { URL_REGEX } from '../../common/constants/index.js'
+import { INTERNET_PROTOCOLS } from './constants.js'
+
 export const checkEntryValidation = {
   createCheckEntry: {
+    body: {
+      name: Joi.string().required(),
+      url: Joi.string().regex(URL_REGEX).required(),
+      protocol: Joi.string().valid(...INTERNET_PROTOCOLS).required(),
+      path: Joi.string(),
+      port: Joi.number(),
+      webhook: Joi.string().regex(URL_REGEX),
+      timeout: Joi.number().required().default(5),
+      interval: Joi.number().required().default(10),
+      threshold: Joi.number().required().default(1),
+      authentication: {
+        username: Joi.string(),
+        password: Joi.string()
+      },
+      httpHeaders: Joi.array().items(Joi.object()),
+      assert: { statusCode: Joi.number() },
 
+      tags: {
+        type: Joi.array().items(Joi.string)
+      },
+
+      ignoreSSL: Joi.boolean()
+    }
   }
 }
