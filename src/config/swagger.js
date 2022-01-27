558c96b53e43b71078091ad51bad396c4cb3e327
manarArabi <manar.araby.ma@gmail.com>
2022-01-27 17:56:53 +0200

    fix style

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -2,7 +2,7 @@ export const swaggerDocument = {
   openapi: '3.0.0',
   info: {
     version: '1.0.0',
-    title: 'Site monitoring api endpoints Documentation',
+    title: 'Site monitoring api endpoints Documentation'
   },
   host: 'localhost:3000',
   basePath: '/',
