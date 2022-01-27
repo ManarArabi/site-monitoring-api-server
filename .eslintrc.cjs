686bf6961790a2bf644f4fc34df3f2dbf919b8f7
manarArabi <manar.araby.ma@gmail.com>
2022-01-27 17:26:48 +0200

    fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,16 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    jest: true,
+    node: true
   },
   extends: [
-    'standard'
+    'standard',
+    'eslint:recommended'
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: 12,
     sourceType: 'module'
   },
   rules: {
