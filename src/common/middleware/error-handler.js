f481f997a85212b1e2a43e0b26f268a67cb23103
manarArabi <manar.araby.ma@gmail.com>
2022-01-27 17:56:29 +0200

    add joi error

--- src/common/middleware/error-handler.js
+++ src/common/middleware/error-handler.js
@@ -1,9 +1,18 @@
 import httpStatus from 'http-status'
 
-const { INTERNAL_SERVER_ERROR } = httpStatus
-export const errorHandler = (error, req, res, next) => {
-  res.status = INTERNAL_SERVER_ERROR
-  res.error = error
+const { INTERNAL_SERVER_ERROR, BAD_REQUEST } = httpStatus
 
-  return res.json('Something Went Wrong')
+export const errorHandler = (error, req, res, next) => {
+  const response = {
+    status: INTERNAL_SERVER_ERROR,
+    meta: { error },
+    message: 'Something Went Wrong'
+  }
+
+  if (error.isJoi) {
+    response.status = BAD_REQUEST
+    response.message = 'Request body is not valid'
+  }
+
+  return res.status(response.status).json(response)
 }
