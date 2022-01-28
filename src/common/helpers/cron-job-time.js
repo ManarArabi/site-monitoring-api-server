ddb3178c90071b8adbf2a2a0975ab5d1721efd07
manarArabi <manar.araby.ma@gmail.com>
2022-01-29 01:46:32 +0200

    add helper to convert minutes to cron time

---
+++ src/common/helpers/cron-job-time.js
@@ -0,0 +1,9 @@
+export const convertMinutesToCronJobTime = minutes => {
+  const hours = Math.floor(minutes / 60)
+  const cronHours = hours === 0 ? '*' : `*/${hours}`
+
+  const minutesReminder = minutes % 60
+  const cronMinutes = minutesReminder === 0 ? '*' : `*/${minutesReminder}`
+
+  return `0 ${cronMinutes} ${cronHours} * * *`
+}
