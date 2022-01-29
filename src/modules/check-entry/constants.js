f3b9b3e1f42b908f4f4dd0359ac8d3be4c9638f5
manarArabi <manar.araby.ma@gmail.com>
2022-01-29 16:22:32 +0200

    disable tcp request temoprarly

--- src/modules/check-entry/constants.js
+++ src/modules/check-entry/constants.js
@@ -1,5 +1,5 @@
 export const HTTP_PROTOCOL = 'http'
 export const HTTPS_PROTOCOL = 'https'
-export const TCP_PROTOCOL = 'tcp'
+// export const TCP_PROTOCOL = 'tcp'
 
-export const INTERNET_PROTOCOLS = [HTTP_PROTOCOL, HTTPS_PROTOCOL, TCP_PROTOCOL]
+export const INTERNET_PROTOCOLS = [HTTP_PROTOCOL, HTTPS_PROTOCOL]
